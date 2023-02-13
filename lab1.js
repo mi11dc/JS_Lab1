c7bba69469b7a47611c54708cf6b19127e7f9477
mi11dc <mi1996.dhaval@gmail.com>
2023-02-13 12:42:39 -0500

    Lab 1 files

---
+++ lab1.js
@@ -0,0 +1,12 @@
+// alert("Hello World");
+
+var age = 26;
+var noOfDaysinYear = 365;
+var totalAgeinDay = age * noOfDaysinYear;
+
+// alert(totalAgeinDay);
+
+var messageTxt1 = "I am";
+var messageTxt2 = "days old...more or less.";
+
+alert(messageTxt1 + ' ' + totalAgeinDay + ' ' + messageTxt2);
